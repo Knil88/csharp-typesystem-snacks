13412f7478d6002839c521bed0c8d7fcf7497e02
Yari <narducciyarigmail.com>
2023-04-05 14:11:34 +0200

    completamento primo snack

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//1-snack creare due numeri e stampare il maggiore 
+
+int numero1;
+int numero2;
+
+Console.WriteLine("Inserisci il primo numero : ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Il primo numero da te immesso è {numero1}");
+
+Console.WriteLine("Inserisci il secondo numero : ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Il Secondo numero da te immesso è {numero2}");
+
+//Creiamo le condizioni che portano a stampare il nummero più grande
+
+if (numero1 > numero2)
+{
+    Console.WriteLine($"il numero più grande è {numero1}");
+}
+
+else if (numero2 > numero1)
+{
+    Console.WriteLine($"il numero più grande è {numero2}");
+}
+else
+{
+    Console.WriteLine("I numeri sono pari");
+}
+
+
+
+
